48d316883caf370595981951bdc69147b6f34dec
Unknown <edmonhong@hotmail.ca>
2018-12-03 02:09:07 -0600

    Sort by ASC
    Sort comments by title, id, content and date posted ASC

--- crud/retrieveComments.php
+++ crud/retrieveComments.php
@@ -7,25 +7,59 @@ function retrieveComments()
     global $db;
     if(isset($_GET['catid'])){
         
-    $catid = $_GET['catid'];
-        if($_GET['sort' == 'id']){
-            $query = "SELECT * FROM comment WHERE com_category_id = {$catid} ORDER BY datetimestamp ASC LIMIT 15";
-    $tables = $db->prepare($query);
-    $tables->execute();
-    $column = $tables->fetchAll();
+        $catid = $_GET['catid'];
+        
+        $query = "SELECT category_name FROM category category_id = {$catid}";
+        $stmt = $db->prepare($query);
+        $stmt->execute();
+        $name = $stmt->fetch();
+        
+        if(isset($_GET['sort'])){
+            if($_GET['sort'] == 'id'){
+                $query = "SELECT * FROM comment WHERE com_category_id = {$catid} ORDER BY commentsID ASC LIMIT 15";
+                $tables = $db->prepare($query);
+                $tables->execute();
+                $column = $tables->fetchAll();
+            }
+        
+      
+            if($_GET['sort'] == 'title'){
+                $query = "SELECT * FROM comment WHERE com_category_id = {$catid} ORDER BY title ASC LIMIT 15";
+                $tables = $db->prepare($query);
+                $tables->execute();
+                $column = $tables->fetchAll();
+            }
+         
+            if($_GET['sort'] == 'content'){
+                $query = "SELECT * FROM comment WHERE com_category_id = {$catid} ORDER BY content ASC LIMIT 15";
+                $tables = $db->prepare($query);
+                $tables->execute();
+                $column = $tables->fetchAll();
+            }
+ 
+            if($_GET['sort'] == 'date'){
+                $query = "SELECT * FROM comment WHERE com_category_id = {$catid} ORDER BY datetimestamp ASC LIMIT 15";
+                $tables = $db->prepare($query);
+                $tables->execute();
+                $column = $tables->fetchAll();
+            }
         }
-    $query = "SELECT * FROM comment WHERE com_category_id = {$catid} ORDER BY datetimestamp ASC LIMIT 15";
-    $tables = $db->prepare($query);
-    $tables->execute();
-    $column = $tables->fetchAll();
+        
+        $query = "SELECT * FROM comment WHERE com_category_id = {$catid} LIMIT 15";
+        $tables = $db->prepare($query);
+        $tables->execute();
+        $column = $tables->fetchAll();
 ?>
 <table class="table table-striped">
+    <caption>
+        <?= $name['category_name']?>
+    </caption>
     <thead class="thead-light">
         <tr>
             <th scope="col"><a href="questionsedit.php?catid=<?= $catid?>&&sort=id">ID</a></th>
             <th scope="col"><a href="questionsedit.php?catid=<?= $catid?>&&sort=title">Title</a></th>
             <th scope="col"><a href="questionsedit.php?catid=<?= $catid?>&&sort=content">Content</a></th>
-            <th scope="col"><a href="questionsedit.php?catid=<?= $catid?>&&sort=date">Date Posted</a></th>
+            <th scope="col"><a href="questionsedit.php?catid=<?= $catid?>&&sort=content">Date Posted</a></th>
             <th scope="col"></th>
         </tr>
     </thead>
@@ -63,5 +97,5 @@ function retrieveComments()
     </tr>
 </table>
 <?php
-}
+    }
 }
